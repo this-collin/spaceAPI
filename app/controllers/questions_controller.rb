7ee2c38c58cc5e5d59d37dd83a270107a963a053
Collin <collincodes9@gmail.com>
2025-05-22 20:39:20 -0500

    feat: added more endpoints for client quiz app

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,5 +15,17 @@ class QuestionsController < ApplicationController
     render json: @questions, status: :ok
   end
 
+  def build_quiz
+    easy_questions = Question.where(difficulty: "easy").sample(5)
+    medium_questions = Question.where(difficulty: "medium").sample(10)
+    hard_questions = Question.where(difficulty: "hard").sample(5)
+
+    render json: {
+      easy_questions: easy_questions,
+      medium_questions: medium_questions,
+      hard_questions: hard_questions
+    }, status: :ok
+  end
+
 
 end
